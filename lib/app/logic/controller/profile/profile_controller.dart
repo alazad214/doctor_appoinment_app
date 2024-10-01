4d806234d1d138da6d5e7c784c532a3470ab6f71
al azad <alazad214@gmail.com>
2024-10-01 22:14:52 +0600

    profile data fetch

---
+++ lib/app/logic/controller/profile/profile_controller.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ProfileController extends GetxController {
+  RxString name = ''.obs;
+  RxString username = ''.obs;
+  RxString email = ''.obs;
+  RxString userId = ''.obs;
+  RxString userImage = ''.obs;
+  RxString phoneNumber = ''.obs;
+  RxString address = ''.obs;
+
+  final auth = FirebaseAuth.instance;
+  final currentUser = FirebaseAuth.instance.currentUser;
+
+  @override
+  void onInit() {
+    super.onInit();
+    if (currentUser != null) {
+      fetchUserData();
+    }
+  }
+
+  fetchUserData() async {
+    if (currentUser != null) {
+      String userEmail = currentUser!.email!;
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .where('email', isEqualTo: userEmail)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        var userData = querySnapshot.docs[0].data() as Map<String, dynamic>;
+        username.value = userData['userName'] ?? '';
+        email.value = userData['email'] ?? '';
+        userImage.value = userData['photos'] ?? '';
+        phoneNumber.value = userData['phone'] ??
+            'https://firebasestorage.googleapis.com/v0/b/arroggo-talk.appspot.com/o/profile.png?alt=media&token=800857a1-ac53-4d13-b427-efb5dfdb6c92';
+        address.value = userData['address'] ?? '';
+      }
+    }
+  }
+}
